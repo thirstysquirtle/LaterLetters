a55dfe2c6cf5971f2880ca592f329c9823515da1
Sin Nguyen <sinnguyen@Sins-MacBook-Air.local>
2024-01-04 01:25:34 -0800

    we cookin

---
+++ src/routes/(auth)/forgot-password/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+
+    let email = "";
+</script>
+
+
+<div class="w-[min(500px,100%)] p-2 sm:p-0 flex flex-col gap-4 mt-28">
+    <h1 class="text-3xl">Receive a Link to Reset your Password</h1>
+    <form on:submit|preventDefault class="flex flex-col text-2xl gap-4 justify-center">
+        <input required placeholder="Email" class="w-full rounded-sm p-1 text-black" id="email" name="email" type="text" bind:value={email} />
+       
+
+        <button class="rounded-sm text-txtSec bg-themeAccent hover:outline p-2" type="submit">Confirm</button>
+    </form>
+    <div class="text-left text-xl">
+        <h2>Looking for the login form? <a class="font-bold" href="/login">Click Here to Login</a></h2>
+        <h2>Do you not have an Account? <a class="font-bold" href="/register">Click Here to Register</a></h2>
+    </div>
+</div>
