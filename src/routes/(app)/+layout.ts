340e58b0058e8c46f9043a1e2a69e6e7011218e8
Sin Nguyen <sinnguyen@Sins-MacBook-Air.local>
2024-02-11 16:38:41 -0800

    Fixed a svelte Oopsie

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -2,19 +2,22 @@ import { Configuration, LettersApi } from "$lib/openapi-fetch";
 import { PUBLIC_API_ENDPOINT } from "$env/static/public";
 import { getUsernameCookie } from "$lib/functions";
 import { goto } from "$app/navigation";
-
+import { browser } from '$app/environment';
 export function load() {
-    const username = getUsernameCookie();
-    if (username === undefined) {
-        goto("/login")
-    }
-    function getApiKey(name: string): string {
-        const lookupTable: { [key: string]: string } = {
-            'X-User-Email': username!
-        }
-        return lookupTable[name];
-    }
 
-    const letters_api = new LettersApi(new Configuration({ basePath: PUBLIC_API_ENDPOINT, apiKey: getApiKey }))
-    return { letters_api }
+    if (browser) {
+        const username = getUsernameCookie();
+        if (username === undefined) {
+            goto("/login")
+        }
+        function getApiKey(name: string): string {
+            const lookupTable: { [key: string]: string } = {
+                'X-User-Email': username!
+            }
+            return lookupTable[name];
+        }
+
+        const letters_api = new LettersApi(new Configuration({ basePath: PUBLIC_API_ENDPOINT, apiKey: getApiKey }))
+        return { letters_api }
+    }
 }
--- src/routes/(not_app)/(auth)/+layout.ts
+++ src/routes/(not_app)/(auth)/+layout.ts
@@ -2,13 +2,15 @@ import { Configuration, AuthApi } from "$lib/openapi-fetch/index";
 import { PUBLIC_API_ENDPOINT } from "$env/static/public";
 import { getUsernameCookie } from "$lib/functions";
 import { goto } from "$app/navigation";
-
+import { browser } from '$app/environment';
 export function load() {
-    if (getUsernameCookie() !== undefined ) {
-        goto("/write");
+    if (browser) {
+        if (getUsernameCookie() !== undefined) {
+            goto("/write");
+        }
+
+
+        const auth_api = new AuthApi(new Configuration({ basePath: PUBLIC_API_ENDPOINT }))
+        return { auth_api }
     }
-
-
-    const auth_api = new AuthApi(new Configuration({ basePath: PUBLIC_API_ENDPOINT }))
-    return { auth_api }
 }
